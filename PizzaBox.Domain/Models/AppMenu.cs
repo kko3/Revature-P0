2b604da7334ce1916a2dd665ef3f9db6bdee3226
kko3 <kko3@zips.uakron.edu>
2019-08-21 23:58:32 -0500

    user login working

--- PizzaBox.Domain/Models/AppMenu.cs
+++ PizzaBox.Domain/Models/AppMenu.cs
@@ -15,45 +15,52 @@ namespace PizzaBox.Domain.Models
     {
       string accountsignedin;
       int choice = -1;
+      bool quit = false;
 
-      accountsignedin = u.AccountName;
 
-      do{
-        Console.WriteLine($"                                            Current User: " + accountsignedin);
-        Console.WriteLine();
-        Console.WriteLine("Welcome To PizzaBox!!!");
-        Console.WriteLine("1. Login to Account.");
-        Console.WriteLine("2. Create an Account.");
-        Console.WriteLine("3. Order");
-        Console.WriteLine("4. Quit");
-        Console.Write("(1-4)? ");
-        choice = Convert.ToInt32(Console.ReadLine());
-      }while(choice < 1 || choice > 4);
 
-      if(choice == 1)
-      {
-        if(u.AccountName == "None")
+      while(!quit){
+        accountsignedin = u.AccountName;
+        
+        do{
+          Console.WriteLine($"                                            Current User: " + accountsignedin);
+          Console.WriteLine();
+          Console.WriteLine("Welcome To PizzaBox!!!");
+          Console.WriteLine("1. Login to Account.");
+          Console.WriteLine("2. Create an Account.");
+          Console.WriteLine("3. Order");
+          Console.WriteLine("4. Quit");
+          Console.Write("(1-4)? ");
+          choice = Convert.ToInt32(Console.ReadLine());
+        }while(choice < 1 || choice > 4);
+
+        if(choice == 1)
         {
-          u = u.UserLogin();
+          if(u.AccountName == "None")
+          {
+            u = u.UserLogin();
+          }
+          else
+          {
+            u.ViewAccountDetails();
+          }
         }
-        else
+        else if(choice == 2)
         {
-          u.ViewAccountDetails();
+          u.CreateLogin();
         }
-      }
-      else if(choice == 2)
-      {
-        u.CreateLogin();
-      }
-      else if(choice == 3)
-      {
-        if(u.AccountName == "None"){
-          Console.WriteLine("Please log in or create an Account.");
-          Menu(u);
-        }
-        else
+        else if(choice == 3)
         {
-          u.PlaceOrder();
+          if(u.AccountName == "None"){
+            Console.WriteLine("Please log in or create an Account.");
+          }
+          else
+          {
+            u.PlaceOrder();
+          }
+        }
+        else{
+          quit = true;
         }
       }
     }
--- PizzaBox.Domain/Models/User.cs
+++ PizzaBox.Domain/Models/User.cs
@@ -7,19 +7,49 @@ namespace PizzaBox.Domain.Models
   {
     public List<Order> OrderHistory { get; set; }
     public string AccountName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
     public Location SelectedLocation { get; set; }
 
 
     public User UserLogin(){
       User u = new User();
 
-      Console.WriteLine("");
+      string typedusername = "";
+      string typedpassword = "";
+
+      while( u.UserName != typedusername || u.Password != typedpassword)
+      {
+        typedusername = "";
+        typedpassword = "";
+
+        Console.WriteLine("What is your username?");
+        typedusername = Console.ReadLine();
+
+        Console.WriteLine("password?");
+        typedpassword = Console.ReadLine();
+
+        if(u.UserName != typedusername || u.Password != typedpassword)
+        {
+          Console.WriteLine();
+          Console.WriteLine("Try Again");
+        }
+      }
+
+      u.AccountName = "Test";
+      Console.WriteLine("Good");
+      return u;
+    }
+    public User UserLogOut(){
+      User u = new User();
 
       return u;
     }
-    public void UserLogOut(){}
 
-    public void ViewAccountDetails(){}
+    public void ViewAccountDetails()
+    {
+      Console.WriteLine("In Account Details");
+    }
     public void CreateLogin(){}
     public void SelectFromLocations(){}
     public void PlaceOrder(){}
@@ -27,6 +57,8 @@ namespace PizzaBox.Domain.Models
     public User()
     {
       AccountName = "None";
+      UserName = "username";
+      Password = "password";
     }
   }
 }
